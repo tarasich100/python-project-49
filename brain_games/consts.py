68e91cac96b38da6d5645c7262ac57bedcbbe4ad
tarasich100 <tarasich100@gmail.com>
2024-04-05 01:00:57 +0500

    fix description for prime-game in consts.py

--- brain_games/consts.py
+++ brain_games/consts.py
@@ -4,8 +4,8 @@ CALC_GAME_DESCRIPTION = 'What is the result of the expression?'
 GCD_GAME_DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 PROGRESSION_GAME_DESCRIPTION = ('What number is missing '
                                 'in the progression?')
-PRIME_GAME_DESCRIPTION = ('Answer "yes" if given '
-                          'number is prime, otherwise answer "no".')
+PRIME_GAME_DESCRIPTION = ('Answer "yes" if given number is prime. '
+                          'Otherwise answer "no".')
 
 MIN_PROGRESSION_LENGTH = 5
 MAX_PROGRESSION_LENGTH = 10
